d47dc44062074304dc9c634afde829dadb0419e7
Arkadii Ivanov <arkann1985@gmail.com>
2022-05-22 21:48:59 +0100

    Refactored Gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 import com.arkivanov.gradle.AndroidConfig
 import com.arkivanov.gradle.PublicationConfig
-import com.arkivanov.gradle.Target
-import com.arkivanov.gradle.named
-import com.arkivanov.gradle.nativeSet
+import com.arkivanov.gradle.ensureUnreachableTasksDisabled
+import com.arkivanov.gradle.iosCompat
+import com.arkivanov.gradle.macosCompat
+import com.arkivanov.gradle.setupDefaults
+import com.arkivanov.gradle.setupDetekt
+import com.arkivanov.gradle.tvosCompat
+import com.arkivanov.gradle.watchosCompat
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -28,59 +32,44 @@ plugins {
     id("com.arkivanov.gradle.setup")
 }
 
-setupAllProjects {
-    multiplatformTargets(
-        Target.Android,
-        Target.Jvm,
-        Target.Js(),
-        Target.Linux,
-        Target.Ios(),
-        Target.WatchOs(),
-        Target.TvOs(),
-        Target.MacOs(),
-    )
+setupDefaults(
+    multiplatformConfigurator = {
+        android()
+        jvm()
+        js(IR) { browser() }
+        linuxX64()
+        iosCompat()
+        watchosCompat()
+        tvosCompat()
+        macosCompat()
+    },
+    androidConfig = AndroidConfig(
+        minSdkVersion = 15,
+        compileSdkVersion = 31,
+        targetSdkVersion = 31,
+    ),
+    publicationConfig = PublicationConfig(
+        group = "com.arkivanov.essenty",
+        version = deps.versions.essenty.get(),
+        projectName = "Essenty",
+        projectDescription = "Essential libraries for Kotlin Multiplatform",
+        projectUrl = "https://github.com/arkivanov/Essenty",
+        scmUrl = "scm:git:git://github.com/arkivanov/Essenty.git",
+        licenseName = "The Apache License, Version 2.0",
+        licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.txt",
+        developerId = "arkivanov",
+        developerName = "Arkadii Ivanov",
+        developerEmail = "arkann1985@gmail.com",
+        signingKey = System.getenv("SIGNING_KEY"),
+        signingPassword = System.getenv("SIGNING_PASSWORD"),
+        repositoryUrl = "https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/${System.getenv("SONATYPE_REPOSITORY_ID")}",
+        repositoryUserName = "arkivanov",
+        repositoryPassword = System.getenv("SONATYPE_PASSWORD"),
+    ),
+)
 
-    multiplatformSourceSets {
-        val nonNative by named(common)
-        val nonAndroid by named(common)
-        val native by named(nonAndroid)
-
-        listOf(jvm, js).dependsOn(nonAndroid)
-        listOf(android, jvm, js).dependsOn(nonNative)
-        nativeSet.dependsOn(native)
-    }
-
-    androidConfig(
-        AndroidConfig(
-            minSdkVersion = 15,
-            compileSdkVersion = 31,
-            targetSdkVersion = 31,
-        )
-    )
-
-    publicationConfig(
-        PublicationConfig(
-            group = "com.arkivanov.essenty",
-            version = deps.versions.essenty.get(),
-            projectName = "Essenty",
-            projectDescription = "Essential libraries for Kotlin Multiplatform",
-            projectUrl = "https://github.com/arkivanov/Essenty",
-            scmUrl = "scm:git:git://github.com/arkivanov/Essenty.git",
-            licenseName = "The Apache License, Version 2.0",
-            licenseUrl = "http://www.apache.org/licenses/LICENSE-2.0.txt",
-            developerId = "arkivanov",
-            developerName = "Arkadii Ivanov",
-            developerEmail = "arkann1985@gmail.com",
-            signingKey = System.getenv("SIGNING_KEY"),
-            signingPassword = System.getenv("SIGNING_PASSWORD"),
-            repositoryUrl = "https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/${System.getenv("SONATYPE_REPOSITORY_ID")}",
-            repositoryUserName = "arkivanov",
-            repositoryPassword = System.getenv("SONATYPE_PASSWORD"),
-        )
-    )
-
-    detekt()
-}
+setupDetekt()
+ensureUnreachableTasksDisabled()
 
 allprojects {
     repositories {
--- utils-internal/build.gradle.kts
+++ utils-internal/build.gradle.kts
@@ -1,10 +1,27 @@
+import com.arkivanov.gradle.bundle
+import com.arkivanov.gradle.dependsOn
+import com.arkivanov.gradle.setupMultiplatform
+import com.arkivanov.gradle.setupPublication
+import com.arkivanov.gradle.setupSourceSets
+
 plugins {
     id("kotlin-multiplatform")
     id("com.android.library")
     id("com.arkivanov.gradle.setup")
 }
 
-setupMultiplatform {
-    targets()
-    publications()
+setupMultiplatform()
+setupPublication()
+
+kotlin {
+    setupSourceSets {
+        val js by bundle()
+        val native by bundle()
+        val nonNative by bundle()
+
+        native dependsOn common
+        nonNative dependsOn common
+        nativeSet dependsOn native
+        (allSet - nativeSet) dependsOn nonNative
+    }
 }
