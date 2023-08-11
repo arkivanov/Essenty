9b76b5c8ef3adbbc37cbc1ba5ce803b56b7415bb
Arkadii Ivanov <arkann1985@gmail.com>
2023-08-11 16:30:32 +0100

    Updated Kotlin to 1.9.0, AGP to 8.0.2, Gradle to 8.2.1

--- instance-keeper/src/androidMain/kotlin/com/arkivanov/essenty/instancekeeper/AndroidExt.kt
+++ instance-keeper/src/androidMain/kotlin/com/arkivanov/essenty/instancekeeper/AndroidExt.kt
@@ -15,7 +15,7 @@ fun InstanceKeeper(viewModelStore: ViewModelStore): InstanceKeeper =
         viewModelStore,
         object : ViewModelProvider.Factory {
             @Suppress("UNCHECKED_CAST")
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T = InstanceKeeperViewModel() as T
+            override fun <T : ViewModel> create(modelClass: Class<T>): T = InstanceKeeperViewModel() as T
         }
     )
         .get<InstanceKeeperViewModel>()
