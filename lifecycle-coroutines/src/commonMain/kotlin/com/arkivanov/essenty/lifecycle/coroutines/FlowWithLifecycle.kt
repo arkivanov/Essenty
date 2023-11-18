20a0ff47fd3cd80e8dcdee86637a7dc73f3c1dfd
Arkadii Ivanov <arkann1985@gmail.com>
2023-11-18 10:25:04 +0000

    Added LifecycleOwner.repeatOnLifecycle

--- lifecycle-coroutines/src/commonMain/kotlin/com/arkivanov/essenty/lifecycle/coroutines/FlowWithLifecycle.kt
+++ lifecycle-coroutines/src/commonMain/kotlin/com/arkivanov/essenty/lifecycle/coroutines/FlowWithLifecycle.kt
@@ -7,8 +7,12 @@ import kotlinx.coroutines.flow.callbackFlow
 import kotlin.coroutines.CoroutineContext
 
 /**
- * Start [Flow] collecting when [Lifecycle.State] is at least as [minActiveState].
- * It stopped collecting if "opposite" [Lifecycle.State] will appear.
+ * [Flow] operator that emits values from this upstream [Flow] when the [lifecycle]
+ * is at least at [minActiveState] state. The emissions will be stopped when the
+ * [lifecycle] state falls below [minActiveState] state.
+ *
+ * See the [AndroidX documentation](https://developer.android.com/reference/kotlin/androidx/lifecycle/package-summary#(kotlinx.coroutines.flow.Flow).flowWithLifecycle(androidx.lifecycle.Lifecycle,androidx.lifecycle.Lifecycle.State))
+ * for more information.
  */
 fun <T> Flow<T>.withLifecycle(
     lifecycle: Lifecycle,
