ed1d6237cdef04c13d607518959df9a2754c0ba5
Arkadii Ivanov <arkann1985@gmail.com>
2024-05-27 12:31:38 +0100

    Use typeOf instead as key of KClass in InstanceKeeper#getOrCreate

--- instance-keeper/src/commonTest/kotlin/com/arkivanov/essenty/instancekeeper/InstanceKeeperExtTest.kt
+++ instance-keeper/src/commonTest/kotlin/com/arkivanov/essenty/instancekeeper/InstanceKeeperExtTest.kt
@@ -1,7 +1,7 @@
 package com.arkivanov.essenty.instancekeeper
 
-import com.arkivanov.essenty.instancekeeper.InstanceKeeper.SimpleInstance
 import kotlin.test.Test
+import kotlin.test.assertNotSame
 import kotlin.test.assertSame
 
 @Suppress("TestFunctionName")
@@ -10,36 +10,72 @@ class InstanceKeeperExtTest {
     private val dispatcher = InstanceKeeperDispatcher()
 
     @Test
-    fun WHEN_getOrCreate_with_key_called_second_time_THEN_returns_same_instance() {
-        val thing1 = dispatcher.getOrCreate(key = "key") { SimpleInstance(Thing()) }.instance
-        val thing2 = dispatcher.getOrCreate(key = "key") { SimpleInstance(Thing()) }.instance
+    fun WHEN_getOrCreate_with_same_key_called_second_time_THEN_returns_same_instance() {
+        val thing1 = dispatcher.getOrCreate(key = "key") { ThingInstance<Int>() }
+        val thing2 = dispatcher.getOrCreate(key = "key") { ThingInstance<Int>() }
 
         assertSame(thing1, thing2)
     }
 
     @Test
-    fun WHEN_getOrCreate_without_key_called_second_time_THEN_returns_same_instance() {
-        val thing1 = dispatcher.getOrCreate { SimpleInstance(Thing()) }.instance
-        val thing2 = dispatcher.getOrCreate { SimpleInstance(Thing()) }.instance
+    fun WHEN_getOrCreate_with_different_key_called_second_time_THEN_returns_new_instance() {
+        val thing1 = dispatcher.getOrCreate(key = "key1") { ThingInstance<Int>() }
+        val thing2 = dispatcher.getOrCreate(key = "key2") { ThingInstance<Int>() }
+
+        assertNotSame(thing1, thing2)
+    }
+
+    @Test
+    fun WHEN_getOrCreate_with_same_type_called_second_time_THEN_returns_same_instance() {
+        val thing1 = dispatcher.getOrCreate { ThingInstance<Int>() }
+        val thing2 = dispatcher.getOrCreate { ThingInstance<Int>() }
 
         assertSame(thing1, thing2)
     }
 
     @Test
-    fun WHEN_getOrCreateSimple_with_key_called_second_time_THEN_returns_same_instance() {
-        val thing1 = dispatcher.getOrCreateSimple(key = "key", factory = ::Thing)
-        val thing2 = dispatcher.getOrCreateSimple(key = "key", factory = ::Thing)
+    fun WHEN_getOrCreate_with_different_type_called_second_time_THEN_returns_new_instance() {
+        val thing1 = dispatcher.getOrCreate { ThingInstance<Int>() }
+        val thing2 = dispatcher.getOrCreate { ThingInstance<Float>() }
+
+        assertNotSame<ThingInstance<*>>(thing1, thing2)
+    }
+
+    @Test
+    fun WHEN_getOrCreateSimple_with_same_key_called_second_time_THEN_returns_same_instance() {
+        val thing1 = dispatcher.getOrCreateSimple(key = "key") { Thing<Int>() }
+        val thing2 = dispatcher.getOrCreateSimple(key = "key") { Thing<Int>() }
 
         assertSame(thing1, thing2)
     }
 
     @Test
-    fun WHEN_getOrCreateSimple_without_key_called_second_time_THEN_returns_same_instance() {
-        val thing1 = dispatcher.getOrCreateSimple(factory = ::Thing)
-        val thing2 = dispatcher.getOrCreateSimple(factory = ::Thing)
+    fun WHEN_getOrCreateSimple_with_different_key_called_second_time_THEN_returns_new_instance() {
+        val thing1 = dispatcher.getOrCreateSimple(key = "key1") { Thing<Int>() }
+        val thing2 = dispatcher.getOrCreateSimple(key = "key2") { Thing<Int>() }
+
+        assertNotSame(thing1, thing2)
+    }
+
+    @Test
+    fun WHEN_getOrCreateSimple_with_same_type_called_second_time_THEN_returns_same_instance() {
+        val thing1 = dispatcher.getOrCreateSimple { Thing<Int>() }
+        val thing2 = dispatcher.getOrCreateSimple { Thing<Int>() }
 
         assertSame(thing1, thing2)
     }
 
-    private class Thing
+    @Test
+    fun WHEN_getOrCreateSimple_with_different_type_called_second_time_THEN_returns_new_instance() {
+        val thing1 = dispatcher.getOrCreateSimple { Thing<Int>() }
+        val thing2 = dispatcher.getOrCreateSimple { Thing<Float>() }
+
+        assertNotSame<Thing<*>>(thing1, thing2)
+    }
+
+    @Suppress("unused")
+    private class Thing<out T>
+
+    @Suppress("unused")
+    private class ThingInstance<out T> : InstanceKeeper.Instance
 }
